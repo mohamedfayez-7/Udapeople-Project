76f47621551e2eb314d1c7413b9a1b59c815379e
Mohamed Fayez <mohamedfayez_7@yahoo.com>
2022-10-30 12:16:50 +0200

    last commit

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -1,11 +1,11 @@
 export NODE_ENV=production
 export VERSION=1
 export TYPEORM_CONNECTION=postgres
-export TYPEORM_MIGRATIONS_DIR=./migrations
-export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
-export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
-export TYPEORM_HOST=localhost
-export TYPEORM_PORT=5532
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_HOST=database-1.cqypaggiyzsc.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=password
 export TYPEORM_DATABASE=glee
